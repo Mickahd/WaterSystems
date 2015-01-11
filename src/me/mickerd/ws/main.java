91483274aa41f14ed9b2c9e1a9e32637801e7e11
Administrator <Administrator@vps19393.public.cloudvps.com>
2015-01-11 11:45:55 +0100

    xd

--- src/me/mickerd/ws/main.java
+++ src/me/mickerd/ws/main.java
@@ -1,5 +1,21 @@
 package me.mickerd.ws;
 
-public class main {
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class main extends JavaPlugin {
+	
+	static Plugin plugin = null;
+	
+	public void onEnable(){
+		plugin = this;
+	}
+	
+	public void onDisable(){
+		plugin = null;
+	}
+
+	public static Plugin getPlugin(){
+		return plugin;
+	}
 }
